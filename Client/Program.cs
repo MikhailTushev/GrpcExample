c11b97ae27feafe02e1a455fc75c84522159c4ff
AzureAD\MikhailTushev <mikhail.tushev@itl.team>
2022-06-10 10:57:12 +0300

    created project

---
+++ Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Grpc.Net.Client;
+using GrpcService1;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
+
+Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new HelloRequest {Name = name});
+Console.WriteLine("Ответ сервера: " + reply.Message);
+Console.ReadKey();
